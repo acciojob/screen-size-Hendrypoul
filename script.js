ca67451b1d69c9b3751bda16391a3773e568d91c
Hendrypoul <106142503+Hendrypoul@users.noreply.github.com>
2023-06-10 12:19:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ function getSize()
 {
 var w = document.documentElement.clientWidth;
 var h = document.documentElement.clientHeight;
- document.getElementById('wh').innerHTML = "<h1>Width: " + w + " â€¢ Height: " + h + "</h1>";
+ document.getElementById('wh').innerHTML = "<h1>Width: " + w + " and Height: " + h + "</h1>";
 }
